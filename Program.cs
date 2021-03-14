8a477d9df68c17ec7672342536acd74299427457
RD-Ricardo <ricardodias169@gmail.com>
2021-03-14 11:26:33 -0300

    Finalizando Nome Comleto

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nome_Completo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobrenome;
+            Console.WriteLine("Digite seu  primerio  nome");
+            nome =  Console.ReadLine();
+            Console.WriteLine("Digite seu sobrenome");
+            sobrenome =  Console.ReadLine();
+            Console.WriteLine($"Nome completo: {nome}{sobrenome}");
+            Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+        }
+    }
+}
